83b7fcd7f3edd45c63a48b9f72b6ec1c6aeb72d5
João Victor da Silva <75917980+Joao-victor-da-silva@users.noreply.github.com>
2025-06-11 09:45:37 -0300

    adicionado tratativa no checkAuth.js para admin

--- Back-end/middleware/checkAuth.js
+++ Back-end/middleware/checkAuth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
-
 const utils = require('../utils/utils');
 
 module.exports = (req, res, next) => {
     const JWT_KEY = utils.JWT_KEY;
-    try
-    {
+    try {
         const token = req.headers.authorization;
         const decodedToken = jwt.verify(token, JWT_KEY);
         req.userData = { id: decodedToken.id };
-        next();
-    }
-    catch(error)
-    {
-        res.status(401).json({mensagem: 'Não autenticado'});
+
+        // Verifica se o usuário decodificado tem a permissão 'Admin'
+        if (decodedToken.permission !== 'Admin') {
+            return res.status(403).json({ mensagem: 'Acesso não autorizado: Requer permissão de Admin' });
+        }
+        
+        next(); // Continua para a próxima função middleware ou rota
+    } catch(error) {
+        // Se o token for inválido ou não existir
+        res.status(401).json({ mensagem: 'Não autenticado: Token inválido ou ausente' });
     }
 }
